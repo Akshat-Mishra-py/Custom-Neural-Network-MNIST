d0c8eaf7fe5280fdcfe012b5646892d29652b109
Akshat Mishra <akshat.mishra2316@gmail.com>
2025-10-29 12:10:12 +0530

    Add: Added structure of the NeuralNetwork class
    Added initialization of neurons, weights and bias of neural network

--- Neurals.py
+++ Neurals.py
@@ -1,24 +1,40 @@
-import numpy
-
-class Neuron():
-    def __init__(self, activation_value : int):
-        # Contains the activation value of the 
-        self.activation_value = activation_value
-
-    def calculateNumber(self, weight, bias):
-        self.activation_value = weight * self.activation_value + bias
-
-    def __add__(self, other):
-        if type(other) == Neuron:
-            return Neuron(self, self.activation_value + other.activation_value)
-        elif type(other) in [int, float]:
-            return Neuron(self, self.activation_value + other)
-
+import numpy as np
 
 class NeuralNetwork():
-    def __init__(self, layer : list, *args):
-        pass
-        
+    def __init__(self, layers : list | tuple, *args):
+        '''
+        layers : list -> Uses a list of undefined length to 
+        figure out dimensions of the Neural Network 
+        Example : [5,6,8] 
+        This will create a 3 Layered Neural Network Creating :
+        5 Neurons -> Input Layer
+        6 Neurons -> 1st Hidden Layer
+        8 Neurons -> Output Layer
+        '''
+        # TODO:Complete this tomorrow
 
-    
+        #Network Matrixes 
+        self.neurons = np.random.rand(*layers)
+        self.weights = self.create_weights(layers) 
+        '''
+        Weight between two layers can be represented as 2d matrix of NxM
+        N -> no. of neurons in first layer
+        M -> no. of neurons in second layer 
+
+        So for completing the structure it will be a list of 2d matrices
+        of dimensions NxM and length L
+        L -> no. of layers - 1
+        '''
+        self.bias = [np.random.rand(layer) for layer in layers]
+
+    def create_weights(self, layers: list|tuple):
+        weight = []
+        for i in range(len(layers)-1):
+            weight.append(np.random.rand(layers[i],layers[i+1]))
+        return weight
+
+            
+
+            
+        
         
